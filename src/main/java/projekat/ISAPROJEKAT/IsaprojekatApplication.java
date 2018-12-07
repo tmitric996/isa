9599d588e75242bc383e6f2da49593dfd54e8eb5
tmitric996 <tmitric996@gmail.com>
2018-12-07 12:39:14 +0100

    prvi komit

---
+++ src/main/java/projekat/ISAPROJEKAT/IsaprojekatApplication.java
@@ -0,0 +1,12 @@
+package projekat.ISAPROJEKAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaprojekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaprojekatApplication.class, args);
+	}
+}
---
+++ src/test/java/projekat/ISAPROJEKAT/IsaprojekatApplicationTests.java
@@ -0,0 +1,16 @@
+package projekat.ISAPROJEKAT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsaprojekatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
